4858f0f69a13e9762412f3649762ea576d485cba
Peter Roh <minchul.roh.peter@gmail.com>
2018-08-21 14:33:35 +0900

    2018.08.21

--- Quick Sort/QuickSort.c
+++ Quick Sort/QuickSort.c
@@ -10,45 +10,26 @@ void Swap(int* A, int* B)
 int Partition(int DataSet[], int Left, int Right)
 {
 	int First = Left;
-	int Pivot;
-	if((DataSet[First] > DataSet[First + 1]) && (DataSet[First + 1] > DataSet[First + 2]))
-	{
-		Swap(&DataSet[First + 1], &DataSet[First]);
-	}
-	else if((DataSet[First] < DataSet[First + 1]) && (DataSet[First + 1] < DataSet[First + 2]))
-	{
-		Swap(&DataSet[First + 1], &DataSet[First]);
-	}
-	else if((DataSet[First] > DataSet[First + 1]) && (DataSet[First + 1] < DataSet[First + 2]))
-		if(DataSet[First] < DataSet[First + 2])
-		{
-			Pivot = DataSet[First];
-		}
-		else
-		{
-			Swap(&DataSet[First + 2], &DataSet[First]);
-		}
-	else if((DataSet[First] < DataSet[First + 1]) && (DataSet[First + 1] > DataSet[First + 2]))
-		if(DataSet[First] < DataSet[First + 2])
-		{
-			Swap(&DataSet[First + 2], &DataSet[First]);
-		}
-		else
-			Pivot = DataSet[First];
+    int Pivot = DataSet[First];
 
-	Pivot = DataSet[First];
 	++Left;
 
 	while(Left <= Right)
 	{
 		while(DataSet[Left] <= Pivot && Left < Right)
 			++Left;
+
 		while(DataSet[Right] > Pivot && Left <= Right)
 			--Right;
+
 		if(Left < Right)
+		{
 			Swap(&DataSet[Left], &DataSet[Right]);
+		}
 		else
+		{
 			break;
+		}
 	}
 
 	Swap(&DataSet[First], &DataSet[Right]);
@@ -58,13 +39,9 @@ int Partition(int DataSet[], int Left, int Right)
 
 void QuickSort(int DataSet[], int Left, int Right)
 {
-	int i;
-
 	if(Left < Right)
 	{
 		int Index = Partition(DataSet, Left, Right);
-
-		for(i = ; i ; i--)
 
 		QuickSort(DataSet, Left, Index - 1);
 		QuickSort(DataSet, Index + 1, Right);
